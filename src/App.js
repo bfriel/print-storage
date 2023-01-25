3c78f4b9c4ed656eee0f22b1739cd0c4cb640596
Brian Friel <briandfriel@gmail.com>
2023-01-24 21:52:27 -0800

    dirty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [storageArrays, setStorageArrays] = useState({
+    localStorage: [],
+    sessionStorage: [],
+  });
+
+  useEffect(() => {
+    const createArray = (storage) => {
+      const array = [];
+      for (let i = 0; i < storage.length; i++) {
+        const key = storage.key(i);
+        const value = storage.getItem(key);
+        array.push({ key, value });
+      }
+      return array;
+    };
+
+    const localStorage = createArray(window.localStorage);
+    const sessionStorage = createArray(window.sessionStorage);
+
+    setStorageArrays({ localStorage, sessionStorage });
+  }, []);
+
+  const listItems = (storage) =>
+    storage.map((item) => (
+      <li key={item.key}>
+        <b>{item.key}: </b>
+        {item.value}
+      </li>
+    ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Local Storage</h1>
+      {listItems(storageArrays.localStorage)}
+      <h1>Session Storage</h1>
+      {listItems(storageArrays.sessionStorage)}
     </div>
   );
 }
